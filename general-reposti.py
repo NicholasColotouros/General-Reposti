e37bfe35ff3a18517d15f56c26c4e712e181c908
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2018-06-02 21:52:39 -0400

    moved secret info to json

--- general-reposti.py
+++ general-reposti.py
@@ -1,10 +1,16 @@
-import discord
-from discord.ext import commands
 import asyncio
+import discord
+import json
 import re
 import redditcmds
 
-TOKEN = 'NDUyNDcwNjUyNTc0NzYwOTgw.DfQ_Vw.irfiJYJDHzEm2B7SN0l4KefnPhg'
+from discord.ext import commands
+
+with open('secret/data.json') as f:
+    data = json.load(f)
+
+BOT_TOKEN = data['discord']['bot-token']
+ADMIN_ID = data['discord']['admin-id']
 client = discord.Client()
 
 bot = commands.Bot(command_prefix='!')
@@ -18,31 +24,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    try:
-        # meme text
-        if re.search('general reposti', message.content, re.IGNORECASE):
-            if re.search('I hate you', message.content, re.IGNORECASE):
-                nameToReplyWith = message.author.name
-                if(message.author.nick):
-                    nameToReplyWith = message.author.nick
-                await client.send_message(message.channel, 'It\'s over ' + nameToReplyWith + '. I have the high ground!')
-            else:
-                await client.send_message(message.channel, 'Hello there!')
+    # meme text
+    if re.search('general reposti', message.content, re.IGNORECASE):
+        if re.search('I hate you', message.content, re.IGNORECASE):
+            nameToReplyWith = message.author.name
+            if(message.author.nick):
+                nameToReplyWith = message.author.nick
+            await client.send_message(message.channel, 'It\'s over ' + nameToReplyWith + '. I have the high ground!')
+        else:
+            await client.send_message(message.channel, 'Hello there!')
 
-        #process actual commands
-        await bot.process_commands(message)
-    except:
-        await client.send_message(message.channel, 'Impossible.\nPerhaps the archives are incomplete.\n<@96369236611457024> might have more info on this issue.')
-        raise
+    #process actual commands
+    await bot.process_commands(message)
 
 @bot.command(description='Gets the top URL post from r/PrequelMemes and posts it', pass_context=True)
 async def shitpost(ctx):
-    await client.send_message(ctx.message.channel, 'doot')
     try:
         shitpostUrl = redditcmds.GetShitPostURL()
         await client.send_message(shitpostUrl)
     except:
-        await client.send_message(ctx.message.channel, 'Impossible.\nPerhaps the archives are incomplete.\n<@96369236611457024> might have more info on this issue.')
+        await client.send_message(ctx.message.channel, 'Impossible.\nPerhaps the archives are incomplete.\n<@' + ADMIN_ID + '> might have more info on this issue.')
         raise
 
-client.run(TOKEN)
+client.run(BOT_TOKEN)

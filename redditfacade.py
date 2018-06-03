24292fa054c783c23fc4c48c5292c7704c5646d3
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2018-06-03 12:28:33 -0400

    !shitpost now gets a random post from /r/prequelmemes

--- redditfacade.py
+++ redditfacade.py
@@ -1,5 +1,8 @@
 import dataloader
 import praw
+import re
+
+from random import randint
 
 reddit = praw.Reddit(client_id=dataloader.redditData['client-id'], 
                     client_secret=dataloader.redditData['client-secret'],
@@ -8,14 +11,13 @@ reddit.read_only = True
 
 
 def GetShitPostURL():
-    print("===================")
-    print(dataloader.redditData['client-id'])
-    print(dataloader.redditData['client-secret'])
-    print(dataloader.redditData['user-agent'])
-    print("===================")
-    submissions = reddit.subreddit('prequelmemes').hot(limit=10)
+    submissions = reddit.subreddit('prequelmemes').hot(limit=25)
 
+    shitpostNumber = randint(0, 9)
     for submission in submissions:
-        if submission.url:
-            return submission.url
+        if submission.url: # no self posts
+            if shitpostNumber > 0:
+                shitpostNumber = shitpostNumber - 1
+            else:
+                return submission.url
     return "ERROR: unable to find a shitpost"

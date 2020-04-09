b13b6860ad53018cce262e821682ea48c941e4b6
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2020-04-09 06:18:58 -0400

    Fixed crashes, added getters for accounts and bets

--- betting.py
+++ betting.py
@@ -18,9 +18,28 @@ class BettingInfo:
         self._contestant1 = ""
         self._contestant2 = ""
 
+    def GetAccount(self, userID : str):
+        if not userID in self._accounts:
+            self._accounts[userID] = float(10000)
+        return self._accounts[userID]
+
+    def GetCurrentBet(self, userID : str):
+        if not self.IsBettingAllowed():
+            return None
+        if userID in self._currentBets[self._contestant1]:
+            return (self._contestant1, self._currentBets[self._contestant1][userID])
+        if userID in self._currentBets[self._contestant2]:
+            return (self._contestant2, self._currentBets[self._contestant2][userID])
+        else:
+            return None
+
+
     def Bet(self, userID : str, contestant : str, amount : float):
         if not userID in self._accounts:
             self._accounts[userID] = float(10000)
+        
+        if not contestant in self._currentBets:
+            self._currentBets[contestant] = {}
         
         self._currentBets[contestant][userID] = amount
 
@@ -30,9 +49,9 @@ class BettingInfo:
     def StartBetting(self, p1 : str, p2 : str):
         # TODO clear existing bets
         # TODO check that betting is not in progress
-        _contestant1 = p1
-        _contestant2 = p2
-        _bettingAllowed = True
+        self._contestant1 = p1
+        self._contestant2 = p2
+        self._bettingAllowed = True
 
     def EndBetting(self):
         # TODO check that betting is in progress
@@ -41,6 +60,9 @@ class BettingInfo:
 
     def IsValidContestant(self, contestant : str):
         return self._contestant1 == contestant or self._contestant2 == contestant
+    
+    def GetCurrentMatchup(self):
+        return self._contestant1 + ' vs ' + self._contestant2
 
 Lock = asyncio.Lock()
 BettingInfo = BettingInfo()
@@ -52,7 +74,7 @@ async def bet(ctx, amount : float, contestant : str):
     try:
         if BettingInfo.IsBettingAllowed():
             if not BettingInfo.IsValidContestant(contestant):
-                await ctx.send(contestant + ' is not a valid contestant. The current matchup is ' + Contestant1 + ' vs ' + Contestant2)
+                await ctx.send(contestant + ' is not a valid contestant. The current matchup is ' + BettingInfo.GetCurrentMatchup())
             else:
                 # TODO trim float to 2 decimals
                 strID = str(ctx.message.author.id)
@@ -63,8 +85,34 @@ async def bet(ctx, amount : float, contestant : str):
     finally:
         Lock.release()
 
-# TODO getters for current bets
-# TODO getters for current accounts
+
+# TODO format the amount as currency
+@reposti.bot.command(pass_context=True)
+async def bet_get_account(ctx):
+    await Lock.acquire()
+    try:
+        strID = str(ctx.message.author.id)
+        amount = BettingInfo.GetAccount(strID)
+        await ctx.send('<@' + strID + '> has $' + str(amount) + ' in their account.')
+    finally:
+        Lock.release()
+
+# TODO format the amount as currency
+@reposti.bot.command(pass_context=True)
+async def bet_get(ctx):
+    await Lock.acquire()
+    try:
+        strID = str(ctx.message.author.id)
+        result = BettingInfo.GetCurrentBet(strID)
+        
+        bet = 'no active bets.'
+        if result is not None:
+            bet = '$' + str(result[1]) + ' on ' + result[0]
+
+        await ctx.send('<@' + strID + '> has ' + bet)
+    finally:
+        Lock.release()
+
 
 ######## Admin commands ########
 @reposti.bot.command(description='(admin) start betting', pass_context=True)
@@ -75,7 +123,7 @@ async def bet_start(ctx, p1 : str, p2 : str):
             await ctx.send('You are on this counsel, but we do not grant you the rank of master.\nOnly<@' + reposti.ADMIN_ID + '> can do this.')
         else:
             BettingInfo.StartBetting(p1, p2)
-            await ctx.send('Taking bets for ' + p1 + ' vs. ' + p2)
+            await ctx.send('Taking bets for ' + BettingInfo.GetCurrentMatchup())
     finally:
         Lock.release()
         

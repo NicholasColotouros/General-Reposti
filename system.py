00c50734f083a3f654324eea4120ac742954a21c
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2020-04-12 00:18:46 -0400

    added general error handling so that there's user feedback

--- system.py
+++ system.py
@@ -3,6 +3,7 @@ import reposti
 import shlex
 import sys
 
+from discord.ext import commands
 from subprocess import run
 
 @reposti.bot.event
@@ -47,3 +48,15 @@ async def reboot(ctx):
         await ctx.send("If you strike me down I shall become more powerful than you can possibly imagine.")
         run(shlex.split(r"""powershell.exe -file "start_bot.ps1" """))
         sys.exit(0)
+
+@reposti.bot.event
+async def on_command_error(ctx, err):
+    ctxstr = 'Error raised by (' + ctx.message.author.name + ') with message (' + ctx.message.content + '):\n'
+    fullerr = ctxstr + str(err) + '\n'
+    print(fullerr) # TODO log the bottom 2 cases to file
+    if isinstance(err, commands.CommandNotFound):
+        return
+    elif isinstance(err, commands.BadArgument):
+        await ctx.send('You had a bad argument when using the command \'' + ctx.command.name + '\'. Try again.')
+    else:
+        await ctx.send('The command was so intense it made me shid. Help me <@' + str(reposti.ADMIN_ID) + '>.\n' + fullerr)
